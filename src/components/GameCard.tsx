bcf851e44da30093328aabe57bc6b324f84e1d40
Sachin Kanna <skanna@enl.mu>
2023-09-06 15:00:18 +0400

    Navigation

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import PlatformsList from './PlatformsList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
 import { Game } from '../interfaces';
+import { Link } from 'react-router-dom';
 
 interface Props {
   game: Game;
@@ -19,7 +20,9 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={'xl'}>{game.name}</Heading>
+        <Heading fontSize={'xl'}>
+          <Link to={`/games/${game.slug}`}>{game.name}</Link>
+        </Heading>
       </CardBody>
     </Card>
   );
